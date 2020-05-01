b4284fe3aacd6d43a8ccccb83c7d3ba5fec75e68
Bryant, Charles <c3bryant@ucsd.edu>
2020-04-30 18:18:02 -0700

    Update constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
-const submit_btn_inactive = " ... Sending";
+const submit_btn_inactive = "Sending";
 const submit_btn_active = "Submit";
-const subit_btn_try_again = "'Try again'";
+const subit_btn_try_again = "Try again";
 const submit_btn_received = "Received";
 
 const flash_off = "FLASH OFF";
